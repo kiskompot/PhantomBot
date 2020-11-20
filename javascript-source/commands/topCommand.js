341a817834623a053df5b2bf673b539c6e5adb39
Ritle <72066591+Ritle@users.noreply.github.com>
2020-11-20 13:43:31 +0300

    Update topCommand.js

--- javascript-source/commands/topCommand.js
+++ javascript-source/commands/topCommand.js
@@ -22,8 +22,8 @@
  Составление и объявление списков самых популярных зрителей по количеству очков и самому большому времени проведенному на канале
  */
 (function() {
-    var amountPoints = $.getSetIniDbNumber('settings', 'topListAmountPoints', 5),//переменная хранящая значение пойнтов
-        amountTime = $.getSetIniDbNumber('settings', 'topListAmountTime', 5);// переменная хранящая сумму времени
+    var amountPoints = $.getSetIniDbNumber('settings', 'topListAmountPoints', 5),//переменная хранящая значение пойнтов variable storing point value
+        amountTime = $.getSetIniDbNumber('settings', 'topListAmountTime', 5);// variable storing the amount of time
 
     /*
      * @function reloadTop - функция перезагрузки топа
@@ -34,34 +34,34 @@
     }
 
     /*
-     * @function getTop5 функция перезагрузки топа зрителей 
+     * @function getTop5 - функция перезагрузки топа зрителей 
      *
      * @param {string} iniName
-     * @returns {Array}
+     * @returns {Array} - возвращает массив пользователей
      */
-    // функция получения первых 5 пользователей топа по времени и очкам
+    // Функция получения первых 5 пользователей топа по времени и очкам - Function of getting the first 5 users of the top by time and points
     function getTop5(iniName) {
         var keys = $.inidb.GetKeysByNumberOrderValue(iniName, '', 'DESC', (iniName.equals('points') ? amountPoints + 2: amountTime + 2), 0),
             list = [],
             i,
             ctr = 0;
-//Цикл создания списка польхзователь по очкам или времени
+//Цикл создания списка польхзователь по очкам или времени - Cycle for creating a list of users by points or time
         for (i in keys) {
             if (!$.isBot(keys[i]) && !$.isOwner(keys[i])) {
                 if (ctr++ == (iniName.equals('points') ? amountPoints : amountTime)) {
                     break;
                 }
-                list.push({
+                list.push({ // Добавление в массив пользователь+значение полученные по ключу Adding user + value to the array obtained by key                
                     username: keys[i],
                     value: $.inidb.get(iniName, keys[i])
                 });
             }
         }
-//Сортировка полученного списка 
+//Сортировка полученного массива Sorting the resulting array
         list.sort(function(a, b) {
             return (b.value - a.value);
         });
-//Вернуть список пользователей взависимости от запроса(время или очки)
+//Вернуть массив пользователей взависимости от запроса(время или очки) Return an array of users depending on the request (time or points)
         if (iniName.equals('points')) {
             return list.slice(0, amountPoints);
         } else {
@@ -86,32 +86,32 @@
             if (!$.bot.isModuleEnabled('./systems/pointSystem.js')) {
                 return;
             }
- // обращение к функции получения первых 5 пользователей по очкам
+ // обращение к функции получения первых 5 пользователей по очкам Access to the function of getting the first 5 users by points
             var temp = getTop5('points'),
                 top = [],
                 i;
- // добавление в массив имен пользоватлей и очков
+ // Добавление в массив имен пользоватлей и очков Adding usernames and points to the array
             for (i in temp) {
                 top.push((parseInt(i) + 1) + '. ' + $.resolveRank(temp[i].username) + ' ' + $.getPointsString(temp[i].value));
             }
-
+//Отобразить список популряных пользователей по очкам Display a list of top users by points
             $.say($.lang.get('top5.default', amountPoints, $.pointNameMultiple, top.join(', ')));
             return;
         }
 
         /*
          * @commandpath toptime - Display the top people with the most time
-         // обращение к функции получения первых 5 пользователей по времени на стриме
+         // Обращение к функции получения первых 5 пользователей по времени на стриме
          */
         if (command.equalsIgnoreCase('toptime')) {
             var temp = getTop5('time'),
                 top = [],
                 i;
-// добавление в массив имен пользоватлей и времени  
+// Добавление в массив имен пользоватлей и времени Adding usernames and time to the array
             for (i in temp) {
                 top.push((parseInt(i) + 1) + '. ' + $.resolveRank(temp[i].username) + ' ' + $.getTimeString(temp[i].value, true));
             }
-
+//Отобразить список популярных зрителей по времени  Display a list of top users by time
             $.say($.lang.get('top5.default', amountTime, 'time', top.join(', ')));
             return;
         }
@@ -153,14 +153,14 @@
         }
 
         /*
-         * @commandpath reloadtopbots - DEPRECATED. Use !reloadbots устарело
+         * @commandpath reloadtopbots - DEPRECATED. Use !reloadbots - устарело
          */
         if (command.equalsIgnoreCase('reloadtopbots')) {
             $.say($.whisperPrefix(sender) + $.lang.get('top5.reloadtopbots'));
         }
 
         /*
-         * Panel command, no command path needed. Вызов функции перезагрузки, если пользователь напишет в чат reloadtop
+         * Panel command, no command path needed. - Вызов функции перезагрузки, если пользователь напишет в чат reloadtop
          */
         if (command.equalsIgnoreCase('reloadtop')) {
             reloadTop();
@@ -169,7 +169,7 @@
 
     /**
      * @event initReady
-      Регистрация команды в чате и передача ее в обработчик команд
+      Регистрация команды в чате и передача ее в обработчик команд - Registering a command in the chat and sending it to the command handler
      */
     $.bind('initReady', function() {
         $.registerChatCommand('./commands/topCommand.js', 'top', 7);
